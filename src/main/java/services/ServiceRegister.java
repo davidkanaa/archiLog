82048a47c7e44c3faa1d6c35ff95fb18adec4129
davidkanaa <david.kanaa@centrale-marseille.fr>
2016-01-29 18:54:32 -0500

    Added the ServiceRegister class.

---
+++ src/main/java/services/ServiceRegister.java
@@ -0,0 +1,29 @@
+package services;
+
+import models.entities.Item;
+
+/**
+ * Created by davidkanaa on 16-01-29.
+ */
+public class ServiceRegister implements ServiceConsumer {
+
+    public Object search(String terms) {
+        return null;
+    }
+
+    public Object addTrackToPlaylist() {
+        return null;
+    }
+
+    public Object removeTrackFromPlaylist() {
+        return null;
+    }
+
+    public Object getItem(Item item) {
+        return null;
+    }
+
+    public Object authenticate() {
+        return null;
+    }
+}
