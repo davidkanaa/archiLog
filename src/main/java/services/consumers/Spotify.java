79dc5db03fa9ee4356b6b1ac53fc5bb48602ee35
davidkanaa <david.kanaa@centrale-marseille.fr>
2016-01-29 20:02:33 -0500

    Created the Spotify api consumer.

---
+++ src/main/java/services/consumers/Spotify.java
@@ -0,0 +1,31 @@
+package services.consumers;
+
+import models.entities.Item;
+import services.AbstractServiceConsumer;
+import services.ServiceConsumer;
+
+/**
+ * Created by davidkanaa on 16-01-29.
+ */
+public class Spotify extends AbstractServiceConsumer implements ServiceConsumer{
+
+    public Object search(String terms) {
+        return null;
+    }
+
+    public Object addTrackToPlaylist() {
+        return null;
+    }
+
+    public Object removeTrackFromPlaylist() {
+        return null;
+    }
+
+    public Object getItem(Item item) {
+        return null;
+    }
+
+    public Object authenticate() {
+        return null;
+    }
+}
