a13b6c44a60b448fe45cad03e30e8f35613600bb
davidkanaa <david.kanaa@centrale-marseille.fr>
2016-02-03 01:50:12 -0500

    Added constructor and methods.

--- src/main/java/modules/player/Player.java
+++ src/main/java/modules/player/Player.java
@@ -1,7 +1,9 @@
 package modules.player;
 
+import models.entities.Playlist;
 import models.entities.Track;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +11,51 @@ import java.util.List;
  */
 public class Player {
 
-    List<Track> queue;
-    int currentPosition;
-    PlayerState state;
+    private List<Track> queue;
+    private int currentPosition;
+    private PlayerState state;
+
+
+
+
+    private Player() {
+        this.queue = new ArrayList<Track>();
+        this.currentPosition = 0;
+        this.state = new Paused();
+    }
+
+
+    public void play(){
+        state.play();
+    }
+    public void next(){
+        // Plays the next track in the queue if state is Playing.
+    }
+    public void previous(){
+        // Plays the previous track in the queue if state is Paused.
+    }
+
+
+    public void addToQueue(Track track){
+        this.queue.add(track);
+    }
+    public void addToQueue(Playlist playlist){
+        this.queue.addAll(playlist.getTracks());
+    }
+
+    public void clearQueue(){
+        this.queue = new ArrayList<Track>(); // resets the queue to an empty array.
+    }
+
+    public List<Track> getQueue() {
+        return queue;
+    }
+
+    public int getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public PlayerState getState() {
+        return state;
+    }
 }
