fce087b4f7f5094e7ea7ca724fc07af7b7cf85dd
davidkanaa <david.kanaa@centrale-marseille.fr>
2016-02-03 01:26:27 -0500

    Created SoundCloud service.

---
+++ src/main/java/services/consumers/SoundCloud.java
@@ -0,0 +1,108 @@
+package services.consumers;
+
+import models.entities.Item;
+import models.entities.Track;
+import services.AbstractServiceConsumer;
+import services.ServiceConsumer;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by davidkanaa on 16-02-02.
+ */
+public class SoundCloud extends AbstractServiceConsumer implements ServiceConsumer {
+
+
+    public SoundCloud() {
+        this.clientID = "39c4a333359cc62c40b9795ad9bb5203";
+        this.baseURL = "http://api.soundcloud.com";
+    }
+
+    public List<Item> search(String terms) {
+
+        String query;
+        URL url;
+        List<Item> results = new ArrayList<Item>();
+
+        try {
+            query = URLEncoder.encode(terms, "UTF-8");
+            url = new URL(baseURL+"/tracks?client_id="+clientID+"&q="+query);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            JsonReader reader = Json.createReader(in);
+            JsonArray jsonResults = reader.readArray();
+
+            // retrieves the tracks within the results.
+            for (JsonObject result:
+                 jsonResults.getValuesAs(JsonObject.class)) {
+
+                Track track = new Track();
+
+                // Sets the album name.
+                String albumName = "n/a";
+                track.setAlbumName(albumName);
+
+                // Sets the user as the sole artist of the track.
+                String user = result.getJsonObject("user").getString("username");
+                List<String> artists = new ArrayList<String>();
+                artists.add(user);
+                track.setArtitsNames(artists);
+
+                // Sets the duration.
+                long duration = result.getJsonNumber("duration").longValue();
+                track.setDuration(duration);
+
+                // Sets title.
+                String title = result.getString("title");
+                track.setTitle(title);
+
+                // Sets uri.
+                String uri = result.getString("stream_url");
+                track.setUri(uri);
+
+                // Sets id.
+                String id = result.getJsonNumber("id").toString();
+                track.setId(id);
+
+                // Sets provider.
+                track.setProvider(this);
+
+                // Adding to the results.
+                results.add(track);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+
+    public Object addTrackToPlaylist() {
+        return null;
+    }
+
+    public Object removeTrackFromPlaylist() {
+        return null;
+    }
+
+    public Object getItem(Item item) {
+        return null;
+    }
+
+    public Object authenticate() {
+        return null;
+    }
+}
