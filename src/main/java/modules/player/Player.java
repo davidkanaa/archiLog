6b520aa135b0ad7f58490ff871d8c6fc989196f4
davidkanaa <david.kanaa@centrale-marseille.fr>
2016-02-02 20:54:30 -0500

    Added  the player module.

---
+++ src/main/java/modules/player/Player.java
@@ -0,0 +1,14 @@
+package modules.player;
+
+import models.entities.Track;
+
+import java.util.List;
+
+/**
+ * Created by davidkanaa on 16-02-02.
+ */
+public class Player {
+
+    List<Track> queue;
+    int currentPosition;
+}
